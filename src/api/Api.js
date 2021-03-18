8097a96be31775dc0aeb181fa8f276b59bf5b5d7
maulayyacyber <ridaulmaulayya@gmail.com>
2021-03-18 08:46:45 +0700

    ubah endpoint API

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'https://donasi.appdev.my.id'
+    baseURL: 'https://donasi.appdev.my.id/api'
 })
 
 export default Api
