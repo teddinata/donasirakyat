3944f395ea1223ff9d9b39702e9fc49ab6991cc7
teddinata <teddinata@users.noreply.github.com>
2021-07-23 16:34:02 +0700

    initial commit

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,14 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'https://donasi.appdev.my.id/api'
+    // baseURL: 'https://donasi.appdev.my.id/api'
+    // baseURL: 'http://donasi.teddinata.com/api'
+    // baseURL: 'http://backend-donasi.test/api'
+    baseURL: 'http://donasi.griyakoscilacap.com/api'
+
+
+
+
 })
 
 export default Api
